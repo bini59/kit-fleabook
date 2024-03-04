92de572174b9012ce052c38cde67d89ebc0b60db
bini59 <myuni0234@gmail.com>
2024-03-04 16:39:26 +0900

    chore: 수정 내용 반영

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -22,11 +22,8 @@
             안녕하십니까 벼룩시장 사이트 개발 관리를 맡은
           </h4>
           <h4 class="subheading">
-            컴퓨터소프트웨어공학과 학생회장 황진권, 컴퓨터공학과 학생회장
-            정민수입니다.
-          </h4>
-          <h4 class="subheading">
-            나뉘어 있던 두 개의 과가 협력하여 의미 있는 프로젝트를 해보았습니다.
+            안녕하세요 벼룩시장  사이트 관리를 맡은 
+            컴퓨터공학과 학생회장 황재연입니다.
           </h4>
           <h4 class="subheading">
             좋은 기회를 주신 학생복지위원장님 그리고 많은 관심을 가져주신 학교
@@ -34,15 +31,14 @@
           </h4>
           <br />
           <h4 class="subheading">
-            안녕하세요 36대 비트윈 학생복지위원회 위원장 김보혁입니다.
+            안녕하세요 39대 "For:You" 학생복지위원회 위원장 최윤성입니다.
           </h4>
           <h4 class="subheading">
             앞으로도 학우분들의 복지를 위해 더 힘쓰는 학생복지위원회가
             되겠습니다.
           </h4>
           <h4 class="subheading">
-            끝으로 벼룩시장 준비에 도움을 주신 컴공, 컴소공 개발자 및 학교
-            선생님들께 감사의 인사드립니다.
+            벼룩시장 준비에 도움을 주신 컴퓨터공학과 개발자 및 학교 선생님들께 감사의 인사드립니다.
           </h4>
         </v-col>
       </v-row>
@@ -53,18 +49,10 @@
     >
       <v-row align="center" justify="center">
         <v-col class="text-center" cols="12">
-          <h1 class="font-weight-black mb-4">2021 Developer</h1>
-          <h4 class="subheading">컴퓨터소프트웨어공학과</h4>
-          <h5 class="subheading font-weight-light mt-2">예약 페이지</h5>
-          <h4 class="subheading">16' 김평안 17' 조우성</h4>
-
-          <h5 class="subheading font-weight-light mt-2">백엔드</h5>
-          <h4 class="subheading">19' 이준재</h4>
-
-          <h1 class="headline- font-weight-black mb-4">.</h1>
+          <h1 class="font-weight-black mb-4">2024 Developer</h1>
           <h4 class="subheading">컴퓨터공학과</h4>
-          <h5 class="subheading font-weight-light mt-2">관리자 페이지</h5>
-          <h4 class="subheading">16' 김도현 16' 권익주</h4>
+          <h5 class="subheading font-weight-light mt-2">관리자</h5>
+          <h4 class="subheading">19' 임유빈</h4>
         </v-col>
       </v-row>
     </v-parallax>
